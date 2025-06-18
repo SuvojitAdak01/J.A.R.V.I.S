dec6273431f17cbc58081803e117f972ee4206c8
Suvojit Adak <suvojitadak360@gmail.com>
2025-06-18 16:12:56 +0530

    Script for trainig the ML model for intent classification

---
+++ train_model.py
@@ -0,0 +1,25 @@
+from jarvis_core.ml.intent_classifier import IntentClassifier
+
+if __name__ == '__main__':
+    # Path of training data
+    training_data_path = 'config/training_data.json'
+    # Path where the trained model will be saved
+    model_save_path = 'jarvis_core/ml/model'
+
+    classifier = IntentClassifier()
+    classifier.train(training_data_path)
+
+    classifier.save_model(model_save_path)
+
+    print("\n--- Testing Model ---")
+    tests = [
+        "what's the time",
+        "weather in Mumbai",
+        "open up my calculator",
+        "who is shah rukh khan",
+        "5 times 10",
+        "turn the sound down"
+    ]
+    for test in tests:
+        intent = classifier.predict(test)
+        print(f"'{test}' -> Predicted Intent: '{intent}'")
